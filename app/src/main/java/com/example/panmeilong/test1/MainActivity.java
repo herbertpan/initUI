a439b2f556f904f76c21704a5c715920476e98cd
123@345.com <123@345.com>
2016-05-31 10:22:13 +0800

    5/31

--- app/src/main/java/com/example/panmeilong/test1/MainActivity.java
+++ app/src/main/java/com/example/panmeilong/test1/MainActivity.java
@@ -6,11 +6,32 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.makeramen.roundedimageview.RoundedImageView;
+
+import butterknife.Bind;
 
 public class MainActivity extends AppCompatActivity {
-    //@Bind(R.id.im_othersinfo_avatar) RoundImageView mRoundImageView;
-
+    @Bind(R.id.tv_othersinfo_name)                      TextView tv_name;
+    @Bind(R.id.im_othersinfo_avatar)                    RoundedImageView im_avatar;
+    @Bind(R.id.tv_othersinfo_num_fans)                  TextView tv_num_fans;
+    @Bind(R.id.tv_othersinfo_num_follow)                TextView tv_num_follows;
+    @Bind(R.id.btn_othersinfo_status)                   Button btn_status;
+    @Bind(R.id.btn_othersinfo_follow)                   Button btn_follow;
+    @Bind(R.id.btn_othersinfo_add_friend)               Button btn_add_friend;
+    @Bind(R.id.tv_othersinfo_sign2)                     TextView tv_sign2;
+    @Bind(R.id.tv_othersinfo_num_created_landmark)      TextView tv_num_created_landmark;
+    @Bind(R.id.tv_othersinfo_created_landmark1)         TextView tv_created_landmark1;
+    @Bind(R.id.tv_othersinfo_created_landmark2)         TextView tv_created_landmark2;
+    @Bind(R.id.tv_othersinfo_num_added_landmark)        TextView tv_num_added_landmark;
+    @Bind(R.id.tv_othersinfo_added_landmark1)           TextView tv_added_landmark1;
+    @Bind(R.id.tv_othersinfo_added_landmark2)           TextView tv_added_landmark2;
+    @Bind(R.id.tv_othersinfo_num_post)                  TextView tv_num_post;
+    @Bind(R.id.tv_othersinfo_post1)                     TextView tv_post1;
+    @Bind(R.id.tv_othersinfo_post2)                     TextView tv_post2;
 
     private DrawerLayout mDrawerLayout;
     private static final String TAG = "testLONG";
@@ -18,9 +39,7 @@ public class MainActivity extends AppCompatActivity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         initToolbar();
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
             localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
@@ -37,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initToolbar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.othersinfo_toolbar);
         if (toolbar != null) {
             setSupportActionBar(toolbar);
             getSupportActionBar().setDisplayShowTitleEnabled(false);
